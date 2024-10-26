e624b716adea049e24a37a888ee0ed8736ed8484
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2024-10-26 23:44:33 +0700

    apply lighting

--- src/init-buffers.js
+++ src/init-buffers.js
@@ -1,17 +1,23 @@
-import { indices } from "./data-indices.js";
-import { positions } from "./data-positions.js";
-import { faceColors } from "./data-colors.js";
+import { indices } from "./data/data-indices.js";
+import { positions } from "./data/data-positions.js";
+import { faceColors } from "./data/data-colors.js";
+import { textureCoordinates } from "./data/data-texture.js";
+import { vertexNormals } from "./data/data-normal.js";
 
 function initBuffers(gl) {
     const positionBuffer = initPositionBuffer(gl);
     const colorBuffer = initColorBuffer(gl);
+    const textureCoordBuffer = initTextureBuffer(gl);
     const indexBuffer = initIndexBuffer(gl);
+    const normalBuffer = initNormalBuffer(gl);
 
     return {
-      position: positionBuffer,
-      color: colorBuffer,
-      indices: indexBuffer,
-    };    
+        position: positionBuffer,
+        normal: normalBuffer,
+        // textureCoord: textureCoordBuffer,
+        color: colorBuffer,
+        indices: indexBuffer,
+    };
 }
 
 function initPositionBuffer(gl) {
@@ -31,9 +37,6 @@ function initPositionBuffer(gl) {
 }
 
 function initColorBuffer(gl) {
-    
-    // Convert the array of colors into a table for all the vertices.
-
     var colors = [];
 
     for (var j = 0; j < faceColors.length; ++j) {
@@ -52,30 +55,40 @@ function initColorBuffer(gl) {
 function initIndexBuffer(gl) {
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
-  
-    // const indices = [
-    //   0, 1, 2, 0, 2, 3, // front
-    //   4, 5, 6, 4, 6, 7, // back
-    //   8, 9, 10, 8, 10, 11, // top
-    //   12, 13, 14, 12, 14, 15, // bottom
-    //   16, 17, 18, 16, 18, 19, // right
-    //   20, 21, 22, 20, 22, 23, // left
-    // ];
-  
-    // Now send the element array to GL
-  
+
     gl.bufferData(
-      gl.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(indices),
-      gl.STATIC_DRAW,
+        gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(indices),
+        gl.STATIC_DRAW
     );
-  
+
     return indexBuffer;
-  }
-  
+}
+
+function initTextureBuffer(gl) {
+    const textureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(textureCoordinates),
+        gl.STATIC_DRAW
+    );
+
+    return textureCoordBuffer;
+}
+
+function initNormalBuffer(gl) {
+    const normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(vertexNormals),
+        gl.STATIC_DRAW
+    );
+
+    return normalBuffer;
+}
 
 export { initBuffers };
